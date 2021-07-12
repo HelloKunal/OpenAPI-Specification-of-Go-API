2970c8198c2ba425239dc1023694da2bcb879045
Kunal <hellokunalpawar@gmail.com>
2021-07-12 21:56:03 +0530

    started testing on actual code

---
+++ swagger-ui/docs.go
@@ -0,0 +1,24 @@
+// Package classification goAPI.
+//
+// Documentation of our go API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: some-url.com
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+package docs
